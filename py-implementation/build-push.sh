7f3ab0d064d2bd74ea2253da015afa9c67a5280e
kinebogin <kinebogin@gmail.com>
2022-09-16 21:24:02 +0400

    build-push.sh: parametrize the docker repo

--- py-implementation/build-push.sh
+++ py-implementation/build-push.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 version=${1}
+repo=${2}
 
 if [ -z "$version" ]
 then
-  echo "Expected a version for the docker image as a parameter"
+  echo "Expected a version for the docker image as a first parameter"
+  exit 1
+fi
+if [ -z "$repo" ]
+then
+  echo "Expected a repository for the docker image as the second parameter"
   exit 1
 fi
 
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v8 -t kinebogin/grigory-py:"$version" --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v8 -t $repo/grigory-py:"$version" --push .
